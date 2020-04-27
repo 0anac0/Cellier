2cb566831c4a027c6e7df227f7da9996bbe08f92
Ana Cláudia <ana.claudia@gocase.com.br>
2020-04-27 00:31:03 -0300

    Vue first setup

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
+
+  
 end
